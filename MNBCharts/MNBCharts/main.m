2b3fd708d4729d3e6d42886cfb324bf30dd53415
Ruben Mendez <ruben.mendez@gmail.com>
2014-02-14 09:02:39 +0100

    initial setup
    Signed-off-by: Ruben Mendez <ruben.mendez@gmail.com>

---
+++ MNBCharts/MNBCharts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MNBCharts
+//
+//  Created by Ruben on 14/02/14.
+//  Copyright (c) 2014 minube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MNBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MNBAppDelegate class]));
+    }
+}
